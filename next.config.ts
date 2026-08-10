882286aad63f9d4bbead18186ed2bd97ffc64166
Sojib Ahmed <sojibahmed.officework@gmail.com>
2026-08-10 11:03:48 +0600

    add image remote patterns configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   reactCompiler: true,
+  
 };
 
 export default nextConfig;
